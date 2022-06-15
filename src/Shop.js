fb03f9e0fda29cc13f8069429b13c54abefc31a7
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2022-06-15 14:36:10 +0300

    Add states and a basic fetch call

--- src/Shop.js
+++ src/Shop.js
@@ -1,5 +1,30 @@
 import React, { useState, useEffect } from "react";
 
 export default function Shop() {
+
+    const [items, setItems] = useState([]);
+    const [name, setName] = useState("");
+    const [desc, setDesc] = useState("");
+    const [loader, setLoader] = useState(true);
+
+    
+
+    useEffect(() => {
+        (async() => {
+            try {
+                const response = await fetch("https://covid-shop-mcs.herokuapp.com");
+                const data = await response.json();
+                if (data) {
+                    setName(data.name);
+                    setDesc(data.desc);
+                }
+            } catch(error) {
+                console.error(error);
+            } finally {
+                setLoader(false);
+            }
+        })()
+    }, [])
+
   return <div className="shop"></div>;
 }
